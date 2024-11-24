bb153dbac1d3b5af6600fbf861ab0854f398017d
Ahmed Abd Ellaziz <99825342+1zizo1@users.noreply.github.com>
2024-11-24 20:46:59 +0200

    attmet 2 ssr for deployment

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,38 +1,25 @@
+// @flow strict
 import { personalData } from "@/utils/data/personal-data";
 
 async function getBlog(slug) {
-  const res = await fetch(`https://dev.to/api/articles/${personalData.devUsername}/${slug}`);
+  const res = await fetch(`https://dev.to/api/articles/${personalData.devUsername}/${slug}`)
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error('Failed to fetch data')
   }
 
   const data = await res.json();
   return data;
 };
 
-export async function getServerSideProps({ params }) {
-  const { slug } = params;
-
-  try {
-    const blog = await getBlog(slug);
-    return { props: { blog } };
-  } catch (error) {
-    console.error(error);
-    return { props: { error: 'Failed to load blog' } };
-  }
-}
-
-function BlogDetails({ blog, error }) {
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
+async function BlogDetails({params}) {
+  const slug = params.slug;
+  const blog = await getBlog(slug);
+ 
   return (
     <div>
-      
     </div>
   );
-}
+};
 
 export default BlogDetails;
