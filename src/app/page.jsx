fb865b877c9a9e8c15c41ab2e0b68741c79431b5
Ahmed Abd Ellaziz <99825342+1zizo1@users.noreply.github.com>
2024-12-06 19:32:57 +0200

    dynamic import

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
+"use client";
+import dynamic from 'next/dynamic';
 
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+// import AboutSection from "./components/homepage/about";
+// import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+// import HeroSection from "./components/homepage/hero-section";
+// import Projects from "./components/homepage/projects";
+// import Skills from "./components/homepage/skills";
 
 // async function getData() {
 //   const res = await fetch(`https://dev.to/api/articles/me/all`,{
@@ -26,8 +28,15 @@ import Skills from "./components/homepage/skills";
 //   return filtered;
 // };
 
-export default async function Home() {
-  // const blogs = await getData();
+// Dynamically import all components with SSR: false to avoid rendering them during SSR
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
+export default function Home() {
 
   return (
     <>
