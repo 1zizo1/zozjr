60c4b2fc75add5ee84f1c109dcc13566ba15e7db
Ahmed Abd Ellaziz <99825342+1zizo1@users.noreply.github.com>
2024-12-06 18:31:23 +0200

    remove scroll to top

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
-import ScrollToTop from "./components/helper/scroll-to-top";
+// import ScrollToTop from "./components/helper/scroll-to-top";
 import React from "react";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
-          <ScrollToTop />
+          {/* <ScrollToTop /> */}
         </main>
  
       </body>
